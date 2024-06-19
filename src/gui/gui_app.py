bc7346d8db4f0cf72eb7499d41317b511e9c566a
Murat Toprak <murattoprak2004@gmail.com>
2024-06-19 22:55:34 +0300

    change the structure of GUI implementation

--- src/gui/gui_app.py
+++ src/gui/gui_app.py
@@ -1,109 +1,140 @@
-from tkinter import filedialog
 import customtkinter as ctk
-
+from tkinter import filedialog
 import time
 
-def new_window():
-    app.withdraw() 
-    new_window = ctk.CTkToplevel(app)  
-    new_window.title("Downloading")
-    new_window.resizable(False,False)
-    new_window.geometry("600x400")
+# TODO: organizing the variables and their names
+# TODO: avoid hardcoding as much as possible
 
-    new_window._set_appearance_mode("light")
+# TODO: implement the bridge between the app and the GUI
 
-    progress_label = ctk.CTkLabel(new_window, text="Downloading...",corner_radius=8,font=("Ariel",25,"bold"))
-    progress_label._set_appearance_mode("light")
-    progress_label.pack(pady=60)
+class FusicApp(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        # Creating the main window of the FUSIC app
+        self.initialize_window()
+        self.create_widgets()
+        self.layout_widgets()
+        
+    def initialize_window(self):
+        # TODO: implement error checking for the parameters
+        title = "FUSIC"
+        window_width = 750
+        window_height = 600
+        app_mode = 'dark'
 
-    progress_bar = ctk.CTkProgressBar(new_window, width=400,height=20, mode="determinate")
-    progress_bar._set_appearance_mode("light")
-    progress_bar.pack(pady=10)
+        self.title(f"{title}")
+        self.geometry(f"{window_width}x{window_height}")
+        self.resizable(False, False)
+        ctk.set_appearance_mode(f"{app_mode}")
 
-    
-    def close_progress_window():
-        new_window.destroy()
+    def create_widgets(self):
+        # TODO: classifying messages
+        # TODO: making variables more organized
 
-    
-    progress_bar.set(0)
-   
-    for i in range(201):
-        time.sleep(0.05)
-        progress_bar.set(i/200)
-        progress_bar.update()
-    progress_label.configure(text="Installed!")
-    new_window.after(1000, close_progress_window)
-    
-    app.deiconify()
-    search_bar.delete(0, ctk.END)
-    entry1.delete(0, ctk.END)
-    error_label_artist.grid_forget()  
-    error_label_directory.grid_forget()
-    app.focus() 
+        artist_err_txt = "You must enter the name of the artist"
+        dir_err_txt    = "You must choose where you want to upload it"
 
-def clicked():
-    if not search_bar.get(): 
-        error_label_artist.grid(column=0, row=6)
-        return  
+        artist_ent_txt = "Enter an artist name: "
+        dir_ent_txt    = "Directory to save lyrics: "
 
-    if not entry1.get():  
-        error_label_directory.grid(column=0, row=6)
-        return 
+        # Crating the main window
+        self.main_window = ctk.CTkFrame(self, fg_color=("gray15", "gray25"), corner_radius=8)
 
-    name_of_artist = search_bar.get()
-    path_of_directory = entry1.get()
-    new_window()
+        # Labels
+        self.error_label_artist    = ctk.CTkLabel(self.main_window, text=artist_err_txt, text_color="red")
+        self.error_label_directory = ctk.CTkLabel(self.main_window, text=dir_err_txt, text_color="red")
+        self.label1 = ctk.CTkLabel(self.main_window, text="Music Downloader", corner_radius=10,
+                                    font=("Calibri", 35, "bold"), text_color="white")
+        self.label2 = ctk.CTkLabel(self.main_window, text="Artist:", corner_radius=10, 
+                                    font=("Calibri", 20, "bold"), text_color="white")
+        self.label3 = ctk.CTkLabel(self.main_window, text="Directory:", corner_radius=10, 
+                                    font=("Calibri", 20, "bold"), text_color="white")
 
-def get_folder_path():
-    folder_path = filedialog.askdirectory(initialdir="/", title="Select a directory")
-    return folder_path
+        # Entries
+        self.artist_entry = ctk.CTkEntry(self.main_window, 
+                        placeholder_text=artist_ent_txt, width=410, height=40)
+        self.dir_entry = ctk.CTkEntry(self.main_window, 
+                        placeholder_text=dir_ent_txt, width=410, height=40)
+        self.dir_entry.bind("<ButtonRelease>", self.select_and_display_path)
+        self.button = ctk.CTkButton(self.main_window, text="Download", fg_color="darkgray", 
+                    text_color="Black", command=self.clicked, width=160, height=40, font=("Ariel", 18))
 
-def select_and_display_path(event):
-    path = get_folder_path()
-    if path:
-        entry1.delete(0, ctk.END)
-        entry1.insert(0, path)
+    def layout_widgets(self):
+        self.main_window.pack(pady=40, padx=80, fill="both", expand=True)
+        self.main_window.grid_columnconfigure(0, weight=1)
 
-app = ctk.CTk()
-app.title("App")
-app.geometry("750x500")
-app.resizable(False,False)
-ctk.set_appearance_mode("dark")
+        for i in range(7):
+            self.main_window.grid_rowconfigure(i, weight=1)
+        
+        self.label1.grid(row=0, column=0)
+        self.label2.grid(row=1, column=0, sticky="ws", padx=83)
+        self.label3.grid(row=3, column=0, sticky="ws", padx=83)
+        self.artist_entry.grid(column=0, row=2, pady=(0, 10))
+        self.dir_entry.grid(column=0, row=4)
+        self.button.grid(column=0, row=5, pady=(15, 0))
 
+    def get_folder_path(self):
+        folder_path = filedialog.askdirectory(initialdir="/", title="Select a directory")
+        return folder_path
 
+    def select_and_display_path(self, event):
+        path = self.get_folder_path()
+        if path:
+            self.dir_entry.delete(0, ctk.END)
+            self.dir_entry.insert(0, path)
 
-window = ctk.CTkFrame(app,fg_color=("gray15", "gray25"),corner_radius=8)
-window.pack(pady = 40,padx = 80,fill = "both",expand ="True")
-window.grid_columnconfigure(0,weight=1)
-for i in range(7):
-    window.grid_rowconfigure(i,weight=1)
+    def close_progress_window(self, window):
+        window.destroy()
 
-error_label_artist = ctk.CTkLabel(window, text="You must enter the name of artist", text_color="red")
-error_label_directory = ctk.CTkLabel(window, text="You must choose where you want to upload it", text_color="red")
+    def download_window(self):
+        self.withdraw()
+        dwn_window = ctk.CTkToplevel(self)
+        dwn_window.title("Downloading")
+        dwn_window.resizable(False, False)
+        dwn_window.geometry("600x400")
 
-label1 = ctk.CTkLabel(window, text="Music Downloader", corner_radius=10, font=("Calibri", 35, "bold"),text_color="white")
-label1.grid(row = 0,column = 0)
+        dwn_window._set_appearance_mode("light")
 
-label2 = ctk.CTkLabel(window, text="Artist:", corner_radius=10, font=("Calibri", 20, "bold"),text_color="white")
-label2.grid(row = 1,column = 0,sticky = "ws",padx = 83)
+        progress_label = ctk.CTkLabel(dwn_window, text="Downloading...", corner_radius=8, font=("Ariel", 25, "bold"))
+        progress_label._set_appearance_mode("light")
+        progress_label.pack(pady=60)
 
-search_bar = ctk.CTkEntry(window, placeholder_text="Enter an artist name:", width=410, height=40)
-search_bar.grid(column= 0,row = 2,pady = (0,10))
+        progress_bar = ctk.CTkProgressBar(dwn_window, width=400, height=20, mode="determinate")
+        progress_bar._set_appearance_mode("light")
+        progress_bar.pack(pady=10)
 
-label3 = ctk.CTkLabel(window, text="Directory:", corner_radius=10, font=("Calibri", 20, "bold"),text_color="white")
-label3.grid(row = 3,column = 0,sticky = "ws",padx = 83)
+        progress_bar.set(0)
 
+        for i in range(201):
+            time.sleep(0.05)
+            progress_bar.set(i/200)
+            progress_bar.update()
 
-entry1 = ctk.CTkEntry(window,placeholder_text="Directory to save lyrics:", width=410, height=40)
-entry1.grid(column= 0,row = 4)
-entry1.bind("<ButtonRelease>", select_and_display_path)
+        progress_label.configure(text="Installed!")
+        dwn_window.after(1000, self.close_progress_window(dwn_window))
 
-# button1 = ctk.CTkButton(window,text="Select",fg_color="darkgray",text_color="Black",width=60,height=40,command =select_and_display_path)
-# button1.grid(column = 1,row=4,sticky = "w")
+        self.deiconify()
+        self.artist_entry.delete(0, ctk.END)
+        self.dir_entry.delete(0, ctk.END)
+        self.error_label_artist.grid_forget()
+        self.error_label_directory.grid_forget()
+        self.focus()
 
-button2 = ctk.CTkButton(window,text="Download",fg_color="darkgray",text_color="Black",command=clicked,width=160,height = 40,font=("Ariel",18))
-button2.grid(column = 0,row = 5,pady=(15,0))
+    def clicked(self):
+        if not self.artist_entry.get():
+            self.error_label_artist.grid(column=0, row=6)
+            return
 
+        if not self.dir_entry.get():
+            self.error_label_directory.grid(column=0, row=6)
+            return
 
+        name_of_artist = self.artist_entry.get()
+        path_of_directory = self.dir_entry.get()
+        self.download_window()
 
-window.mainloop()
+# Running the app
+# if __name__ == "__main__":
+#     app = FusicApp()
+#     app.mainloop()
